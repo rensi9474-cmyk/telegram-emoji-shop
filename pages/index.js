04f31b6967606484987657e21d2539ccead256fb
rensi9474-cmyk <rensi9474@gmail.com>
2025-12-03 15:43:37 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+{
+  "name": "emoji-shop-miniapp",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "14.0.0",
+    "react": "18.2.0",
+    "react-dom": "18.2.0",
+    "react-color": "^2.19.3",
+    "telegram-web-app": "^1.0.0"
+  }
+}
